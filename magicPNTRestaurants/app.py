f95d043e723c617c28924cfd3345d392f1d58267
jorgeho1995 <jorgeho1995@gmail.com>
2022-04-15 15:27:15 +0200

    Created GET request for restaurants. Added images, favicon

--- magicPNTRestaurants/app.py
+++ magicPNTRestaurants/app.py
@@ -1,3 +1,4 @@
+# Imports
 from flask import Flask, request, jsonify, render_template, make_response
 from firebase_admin import credentials, firestore, initialize_app
 import os
@@ -14,11 +15,19 @@ rest_ref = db.collection('restaurants')
 # Index
 @app.route('/')
 def index():
+    """
+        Return index webpage
+    """
     return render_template('index.html')
 
 # Add restaurants to DB
 @app.route('/add', methods=['POST'])
 def create():
+    """
+        Add entry to Cloud Firestore collection
+        Ensure you pass a custom ID as part of json body in post request
+        Example: json={'id': '1', 'coord': [40.0, -3.0], 'title': 'Restaurant 1' ...}
+    """
     try:
         id = request.json['id']
         rest_ref.document(id).set(request.json)
@@ -29,6 +38,11 @@ def create():
 # Read restaurants from DB
 @app.route('/list', methods=['GET'])
 def read():
+    """
+        Get entries from Firestore collection as JSON
+        rest : Return document that matches query ID
+        all_rest : Return all documents
+    """
     try:
         # Check if ID was passed to URL query
         rest_id = request.args.get('id')    
@@ -44,6 +58,11 @@ def read():
 # Update restaurants in DB
 @app.route('/update', methods=['POST', 'PUT'])
 def update():
+    """
+        Update entry in Firestore collection
+        Ensure you pass a custom ID as part of json body in post request
+        Example: json={'id': '1', 'coord': [40.0, -3.0], 'title': 'Restaurant 2' ...}
+    """
     try:
         id = request.json['id']
         rest_ref.document(id).update(request.json)
@@ -54,6 +73,9 @@ def update():
 # Delete restaurants from DB
 @app.route('/delete', methods=['GET', 'DELETE'])
 def delete():
+    """
+        Delete an entry Firestore collection
+    """
     try:
         # Check for ID in URL query
         rest_id = request.args.get('id')
--- magicPNTRestaurants/static/mapbox.js
+++ magicPNTRestaurants/static/mapbox.js
@@ -3,37 +3,73 @@ const CENTER = [-3.7073944, 40.6042722];
 const INIT_ZOOM = 13;
 const INIT_PITCH = 60;
 const API_KEY = 'pk.eyJ1Ijoiam9yZ2VobzE5OTUiLCJhIjoiY2poYnF2cHhqMDMxejMwbzY4dWt0Mmw3YSJ9.6veUQBwHSWY6BQvvfDKX2A';
+const URL_GET = 'http://localhost:8080/list';
+// Global variables
+let map;
 
-// Create Map
-mapboxgl.accessToken = API_KEY;
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: CENTER,
-  zoom: INIT_ZOOM,
-  pitch: INIT_PITCH
-});
+// Run javascript
+window.onload = function() {
+  onLoadWeb();
+};
 
-// Add zoom and rotation controls to the map.
-map.addControl(
-  new mapboxgl.NavigationControl(),
-  'bottom-right'
-);
+// Load function
+function onLoadWeb() {
+  // Create map
+  createMap();
+  // Add restaurants to map
+  loadRestaurants();
+}
 
-// Add directions control
-map.addControl(
-  new MapboxDirections({
-    accessToken: mapboxgl.accessToken
-  }),
-  'top-left'
-);
+// Function to create map
+function createMap() {
+  // Create map
+  mapboxgl.accessToken = API_KEY;
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: CENTER,
+    zoom: INIT_ZOOM,
+    pitch: INIT_PITCH
+  });
+  // Add zoom and rotation controls to the map.
+  map.addControl(
+    new mapboxgl.NavigationControl(),
+    'bottom-right'
+  );
+  // Add directions control
+  map.addControl(
+    new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+  );
+}
+
+// Function to load Restaurants to map
+function loadRestaurants() {
+  // Get all restaurants
+  let rest;
+  fetch(URL_GET)
+    .then(data => { return data.json() })
+    .then(res => { addRestaurantsToMap(res); })
+}
+
+// Function to add restaurants to map
+function addRestaurantsToMap(rest) {
+  let marker;
+  for (let i = 0; i < rest.length; i++) {
+    marker = new mapboxgl.Marker()
+      .setLngLat(rest[i]['coord'])
+      .addTo(map)
+  }
+}
 
 // Function to open/close sidebars
 function toggleSidebar(id) {
   const elem = document.getElementById(id);
   const collapsed = elem.classList.toggle('collapsed');
   const padding = {};
-  padding[id] = collapsed ? 0 : 300; 
+  padding[id] = collapsed ? 0 : 300;
   map.easeTo({
     padding: padding,
     duration: 1000
