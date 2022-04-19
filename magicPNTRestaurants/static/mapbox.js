2a07a6fcff236c6c21df43679966f7db3d9d756e
jorgeho1995 <jorgeho1995@gmail.com>
2022-04-19 18:19:26 +0200

    Some style changes

--- magicPNTRestaurants/static/mapbox.js
+++ magicPNTRestaurants/static/mapbox.js
@@ -8,7 +8,7 @@ const URL_GET = 'http://localhost:8080/list';
 let map;
 
 // Run javascript
-window.onload = function() {
+window.onload = function () {
   onLoadWeb();
 };
 
@@ -41,7 +41,7 @@ function createMap() {
     new MapboxDirections({
       accessToken: mapboxgl.accessToken
     }),
-    'top-left'
+    'top-right'
   );
 }
 
@@ -51,7 +51,7 @@ function loadRestaurants() {
   let rest;
   fetch(URL_GET)
     .then(data => { return data.json() })
-    .then(res => { addRestaurantsToMap(res); })
+    .then(res => { addRestaurantsToMap(res); addRestaurantsToSidebar(res); })
 }
 
 // Function to add restaurants to map
@@ -64,14 +64,34 @@ function addRestaurantsToMap(rest) {
   }
 }
 
-// Function to open/close sidebars
-function toggleSidebar(id) {
-  const elem = document.getElementById(id);
-  const collapsed = elem.classList.toggle('collapsed');
-  const padding = {};
-  padding[id] = collapsed ? 0 : 300;
-  map.easeTo({
-    padding: padding,
-    duration: 1000
-  });
+// Function to add restaurants to sidebar
+function addRestaurantsToSidebar(rest) {
+  let card;
+  for (let i = 0; i < rest.length; i++) {
+    card = '<div class="mdc-card"> ' +
+      ' <div class="mdc-card__primary-action">' +
+      '   <div class="mdc-card__media mdc-card__media--square">' +
+      '     <div class="mdc-card__media-content">' + rest[i]['name'] + '</div>' +
+      '   </div>' +
+      '   <div class="mdc-card__ripple"></div>' + rest[i]['description'] +
+      ' </div>' +
+      ' <div class="mdc-card__actions">' +
+      '   <div class="mdc-card__action-buttons">' +
+      '     <button class="mdc-button mdc-card__action mdc-card__action--button">' +
+      '       <div class="mdc-button__ripple"></div>' +
+      '       <span class="mdc-button__label">Go to</span>' +
+      '     </button>' +
+      '     <button class="mdc-button mdc-card__action mdc-card__action--button">' +
+      '       <div class="mdc-button__ripple"></div>' +
+      '       <span class="mdc-button__label">Directions</span>' +
+      '     </button>' +
+      '   </div>' +
+      '   <div class="mdc-card__action-icons">' +
+      '     <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="Share">share</button>' +
+      '     <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="More options">more_vert</button>' +
+      '   </div>' +
+      ' </div>' +
+      '</div>';
+      //document.getElementById("restaurants").innerHTML += card;
+  }
 }
--- magicPNTRestaurants/static/material.js
+++ magicPNTRestaurants/static/material.js
@@ -1,6 +1,5 @@
 // Create cards
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__actions';
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__actions,  .mdc-card__primary-action';
 const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-    console.log(el);
-    return new mdc.ripples.MDCRipple(el);
+    //return new mdc.ripples.MDCRipple(el);
 });
